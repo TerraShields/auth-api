cfa8906c0b57a056d764d51a294a7a7ab3993b24
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-05-26 22:26:40 +0700

    update dockerfile

--- src/app/tokenHandler.js
+++ src/app/tokenHandler.js
@@ -1,10 +1,6 @@
 import jwt from "jsonwebtoken";
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config();
 
 export const generateToken = (user_id) => {
