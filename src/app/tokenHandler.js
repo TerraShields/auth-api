b6ed78ff349976cc830f0a4bc27823667f0fee3a
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-05-26 22:01:21 +0700

    update dotenv

--- src/app/tokenHandler.js
+++ src/app/tokenHandler.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: `${__dirname}/../../.env` });
+dotenv.config();
 
 export const generateToken = (user_id) => {
 	return jwt.sign({ user_id }, process.env.JWT_SECRET, {
