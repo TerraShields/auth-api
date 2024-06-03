8087425990eed31e3501758b518bda33a5819ab7
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-06-03 10:36:35 +0700

    update oauth callback url

--- src/app/oauth.js
+++ src/app/oauth.js
@@ -3,7 +3,7 @@ import { google } from "googleapis";
 export const oauth2Client = new google.auth.OAuth2(
 	process.env.GOOGLE_CLIENT_ID,
 	process.env.GOOGLE_CLIENT_SECRET,
-	"http://localhost:8080/api/auth/google/callback"
+	"https://api-service-backend-tpercgplna-uc.a.run.app/api/auth/google/callback"
 );
 
 const scopes = [
